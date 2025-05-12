2b6d90632a8cc558ac51888476a6b54322858433
Ahmad Umar <ahmad.umar.byl@gmail.com>
2025-05-13 00:28:22 +0700

    refactor router middleware

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,17 +1,19 @@
-import express from 'express';
+import express from "express";
 import {
   fetchUserData,
   updateUserData,
   fetchUserList,
-  createUser
-} from '../controllers/user';
-import { authMiddleware } from '../middleware/authMiddleware';
+  createUser,
+} from "../controllers/user";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
-router.get('/user/:id', authMiddleware, fetchUserData);
-router.put('/user/:id', authMiddleware, updateUserData);
-router.get('/users', authMiddleware, fetchUserList);
-router.post('/user', authMiddleware, createUser);
+router.use(authMiddleware);
+
+router.get("/user/:id", fetchUserData);
+router.put("/user/:id", updateUserData);
+router.get("/users", fetchUserList);
+router.post("/user", createUser);
 
 export default router;
